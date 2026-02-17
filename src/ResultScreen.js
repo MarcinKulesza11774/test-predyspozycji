5fb85e0cedde7f8ce76f3f11b794514a3a4759cb
Marcin-Kulesza <kuleszamarcin.pl@gmail.com>
2026-02-17 20:30:16 +0100

    full functionality

--- src/ResultScreen.js
+++ src/ResultScreen.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import './App.css';
+import { results } from "./Results.js";
 
 class ResultScreen extends React.Component {
     render() {
 
         const { answersCounter } = this.props;
 
-        const resultTexts = [
-            "A – profil analityczno-informatyczny",
-            "B – profil humanistyczno-językowy",
-            "C – profil techniczno-praktyczny",
-            "D – profil społeczno-usługowy"
-        ];
-
         const maxIndex = answersCounter.indexOf(Math.max(...answersCounter));
+        const finalResult = results[maxIndex];
 
         return (
             <div className="ResultScreen">
-                <h1>Twój wynik:</h1>
-                <p>{resultTexts[maxIndex]}</p>
+                <h1>{finalResult.resultTitle}</h1>
+                <h3>{finalResult.resultContent}</h3>
+                <p>Wynik testu ma charakter pomocniczy i służy wsparciu ucznia w rozpoznaniu jego zainteresowań oraz predyspozycji edukacyjno-zawodowych.  
+W celu pogłębienia wyników oraz omówienia indywidualnych predyspozycji, zainteresowań i możliwości edukacyjnych istnieje możliwość konsultacji z doradcą zawodowym szkolnym lub doradcą zawodowym w Poradni Psychologiczno-Pedagogicznej. 
+Opracowała: doradca zawodowy ZPPP: Katarzyna Bukalska </p>
             </div>
         );
     }
